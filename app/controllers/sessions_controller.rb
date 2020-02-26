05d4cf10ebf96ede6dad1e9673f168962b1aeed1
BlythePollard <github email address>
2020-02-25 19:22:10 -0700

    added validations to user, validations to session (not working yet)

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,16 @@
 class SessionsController < ApplicationController
     def new
+        @user = User.new
     end
 
     def create
-            @user = User.find_by(name: params[:name])
-            if @user = @user.authenticate(params[:password])
-                session[:user_id] = @user.id
-                redirect_to user_path(@user.id)
-            else 
-                redirect_to '/'
-            end
+        #issues with showing errors in validation with login
+        @user = User.find_by(name: params[:name])
+        if @user && @user.authenticate(params[:password])
+            session[:user_id] = @user.id
+            redirect_to user_path(@user.id)
+        else 
+            render :new
         end
     end
 
--- app/models/session.rb
+++ app/models/session.rb
@@ -1,12 +1,8 @@
-class User < ApplicationRecord
+class Session < ApplicationRecord
     has_secure_password 
-
-    has_many :user_activities
-    has_many :activities, through: :user_activities
 
     validates :name, presence: true
     validates :password, presence:true
-    validates :name, uniqueness: true
     validates :name, length: { minimum: 2 }
     validates :password, length: { in: 6..20 }
 end
