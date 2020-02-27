6722dc93dafc1701f7433e53718c36b161b393c0
BlythePollard <github email address>
2020-02-26 21:50:26 -0700

    refactored omniauth find_or_create to User.rb

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,8 +5,8 @@ class SessionsController < ApplicationController
 
     def create
         if auth_hash = request.env["omniauth.auth"]
-            user = User.find_or_create_by_omniauth(auth_hash)
-            session[:user_id] = user.iod
+            user = User.from_omniauth(auth_hash)
+            session[:user_id] = user.id
             redirect_to user_path(user.id)
         else 
             user = User.find_by(name: params[:name])
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,14 +10,12 @@ class User < ApplicationRecord
     validates :name, length: { minimum: 2 }
     validates :password, length: { in: 6..20 }
 
-    def self.find_or_create_by_omniauth(auth_hash)
-        oauth_email = auth_hash["info"]["email"]
-        if user = User.find_by(:email => oauth_email)
-            #if find user, return user
-        else 
-            user = User.create(:email => oauth_email)
-                session[:user_id] = user.id
-                redirect_to user_path(user.id)
+    def self.from_omniauth(auth_hash)
+        self.where(:email => auth_hash["info"]["email"]).first_or_create do |user|
+            user.password = SecureRandom.hex
         end
     end
 end
+
+
+
