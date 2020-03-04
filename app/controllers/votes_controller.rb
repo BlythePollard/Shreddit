aea192b2a7abb43c530ff6f88c9d33bf501d2993
BlythePollard <github email address>
2020-03-03 19:43:07 -0700

    modified user model from has_many :votes to has_many :votes, through: :activities. will need to modify votes#create & view accordingly

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -2,6 +2,7 @@ class VotesController < ApplicationController
     before_action :find_activity
 
     def create
+        #how to modify this so that it goes through user & view does accordingly?
         @a = @activity.votes.new(user_id: current_user.id)
         @a.likes = 1
         @a.save
