53144395eb7cecd6ed33fd185f98364fac5cdb53
BlythePollard <github email address>
2020-03-03 18:36:14 -0700

    added partial for activities/new and activities/edit

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,13 +8,9 @@ class SessionsController < ApplicationController
     def create
         if auth_hash = request.env["omniauth.auth"]
             user = User.from_omniauth(auth_hash)
-            #AUTH_HASH coming through, but email is nil....
             session[:user_id] = user.id
             redirect_to user_path(user.id)
         else 
-            # how to check validations too???
-            # @user = User.new(user_params)
-            # if @user.save
             @user = User.find_by(name: params[:name])
             if user && user.authenticate(params[:password])
                 session[:user_id] = user.id
